57e3f8233df76191539fde4dc8905da68c7c2d41
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-07-10 17:03:28 +0100

    Slider 1 integration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
+  // Activate slider
+  let slider = tns({
+    container: ".my-slider",
+    items: 1,
+    speed: 1000,
+    slideBy: "page",
+    autoplay: false,
+    autoplayButtonOutput: false,
+    loop: true,
+    mouseDrag: true,
+    responsive: {
+      768: {
+        disable: true,
+      },
+    },
+    controls: false,
+    // controlsContainer: ".my-slider-controls",
+  });
+  // After Image
   $(".ab-img").after('<div class="ab-img-after" />');
 });
